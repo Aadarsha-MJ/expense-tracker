c7a475cf30cd2dc1eb5fcd307b7dc1362e95cbb1
Aadarsha Maharjan <maharjanaadarsha227@gmail.com>
2025-01-23 23:37:58 +0100

    added route to transactions

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import express from "express";
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
+import transactionRoutes from "./routes/transactionRoutes.js";
 
 dotenv.config(); // Loading environment variables from .env file
 
@@ -13,13 +14,15 @@ app.use(express.json()); // Middleware to parse incoming requests as JSON
 
 // Routes
 app.use("/api/users", userRoutes);
+app.use("/api/transactions", transactionRoutes);
 
+// Default route
 app.get("/", (req, res) => {
-  res.send("API is running... woohoooo");
+  res.send("API is running...");
 });
 
+// server setup
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
